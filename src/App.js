e325e5bdaecd6ebb12d18747cde7d8ef8e6957ea
AlexAutomation <80054118+AlexicGarcode@users.noreply.github.com>
2022-12-21 20:54:56 -0600

    se actualiza archivo AppRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import AppRouter from "./routes/AppRouter";
 import { UserProvider } from "./context/UserProvider";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import ModelProvider from './context/ModelProvider';
+import 'animate.css';
 
 function App() {
   return (
-    <>      
+    <>
       <UserProvider>
-        <PayPalScriptProvider options={{ "client-id": "AQAHPo_cOwFlj1iJ0svHUchANLemrfnsg6MrKp9Y9k31KGt-pZNRURS_IzRDkLAbe0zoS32Q6_znn7uF" }}>
+      <PayPalScriptProvider options={{ "client-id": "AQAHPo_cOwFlj1iJ0svHUchANLemrfnsg6MrKp9Y9k31KGt-pZNRURS_IzRDkLAbe0zoS32Q6_znn7uF" }}>
+          <ModelProvider>
           <AppRouter />
+          </ModelProvider>
         </PayPalScriptProvider>
       </UserProvider>
     </>
--- src/types/types.js
+++ src/types/types.js
@@ -1,8 +1,9 @@
 export const types  = {
-    GET_PRODUCTS : '[Product] GET_PRODUCTS',
-    GET_PRODUCT : '[Product] GET_PRODUCT',
-    ADD_PRODUCT_CAR : '[Product] ADD_PRODUCT_CAR',
-    DELETE_PRODUCT_CAR : '[Product] DELETE_PRODUCT_CAR'
-
-
+    GET_MODELS : '[Model] GET_MODELS',
+    GET_MODEL : '[Model] GET_MODEL',
+    GET_MODELS_TOTAL : '[Model] GET_MODELS_TOTAL',
+    GET_MODELS_TOTAL_PAGES : '[Model] GET_MODELS_TOTAL_PAGES',
+    ADD_MODEL_CART : '[Model] ADD_MODEL_CART',
+    DELETE_MODEL_CART : '[Model] DELETE_MODEL_CART',
+    EMPTY_CART :'[Model] EMPTY_CART',
 }
